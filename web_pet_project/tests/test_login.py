8dac01881097f03bc9789d3b69c4ca8ff3125bd9
IvanDemianchuk <energy.redler@gmail.com>
2023-02-13 12:34:05 +0300

    - added test design to the tests of login page.

--- web_pet_project/tests/test_login.py
+++ web_pet_project/tests/test_login.py
@@ -9,6 +9,13 @@ from web_pet_project.confest import driver
 @allure.feature('authorize of user')
 @allure.step('Perform success login')
 def test_login_success(driver):
+    """
+    1. Navigate to base url
+    2. Enter 'tomsmith'
+    3. Enter 'SuperSecretPassword!' as password
+    4. Click 'Login' button
+    Verify that the 'You logged into a secure area!' text is displayed and there is 'Logout' button.
+    """
     driver.get(base_url)
     base_locator_instance.auth_locator(driver).click()
     auth_instance.username_locator(driver).send_keys('tomsmith')
@@ -17,15 +24,27 @@ def test_login_success(driver):
     driver.quit()
 
 
+
 @allure.description('this test verifies that user cant enter in the own account without right credentials')
 @allure.severity(allure.severity_level.BLOCKER)
 @allure.epic('Custom_Epic')
 @allure.feature('authorize of user')
 @allure.step('Perform failed login')
 def test_login_fail(driver):
+    """
+    1. Navigate to base url
+    2. Enter the 'invalid_username' value
+    3. Enter the 'invalid_password' value
+    4. Click 'Login' button
+    Verify that the 'You logged into a secure area!' is not displayed.
+    Verify that 'Logout' button is absent.
+    Verify that there is 'Login' button.
+    Verify that 'Your username is invalid' text is displayed.
+    """
     driver.get(base_url)
     base_locator_instance.auth_locator(driver).click()
     auth_instance.username_locator(driver).send_keys('invalid_username')
     auth_instance.password_locator(driver).send_keys('invalid_password')
     auth_instance.submit_button(driver).click()
+    time.sleep(10)
     driver.quit()
